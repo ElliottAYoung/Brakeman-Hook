024251250cb6668c13828dc522692258006891df
ElliottAYoung <elliott.a.young@gmail.com>
2016-04-18 17:28:27 -0500

    Script

---
+++ README.md
@@ -0,0 +1,2 @@
+# Brakeman-Hook
+A pre-push Brakeman Security Scanner global git Hook that I use. Regardless of the outcome it will not prevent or disrupt the push.
---
+++ hook-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global init.templatedir '~/.git-templates'
+mkdir -p ~/.git-templates/hooks
+cp pre-push ~/.git-templates/hooks/
+chmod a+x ~/.git-templates/hooks/pre-push
